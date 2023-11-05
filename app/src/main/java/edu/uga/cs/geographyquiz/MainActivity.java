f052cda9c6557946dca8848742beec4d966913e6
tmoth <tmoth@uga.edu>
2023-11-04 23:32:06 -0400

    A lot of placeholders I will update. Past quiz button not implemented. Swiping not implemented.

--- app/src/main/java/edu/uga/cs/geographyquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/geographyquiz/MainActivity.java
@@ -1,16 +1,30 @@
 package edu.uga.cs.geographyquiz;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String DEBUG_TAG = "Country Quiz";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(DEBUG_TAG, "MainActivity.onCreate(): savedInstanceState: " + savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        DBHelper dbHelper = DBHelper.getInstance(getApplicationContext());
+        //create a toolbar
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setTitle("Take me back!");
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 }
